fc8f336f4d4006b9fc68cd0e5db0d0abc7f42423
Alexander Knapstein <alexander.knapstein@inspirationlabs.com>
2014-09-16 23:45:30 +0200

    wishlist
    Signed-off-by: Alexander Knapstein <alexander.knapstein@inspirationlabs.com>

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ var wishlist = function() {
       name: { type: String, list: true, trim: true},
       creation: { type: Date, 'default': Date.now, form: {readonly: true}},
       entries: {
-
       },
       listelements: [{type: elements.ObjectId}]
     };
@@ -61,31 +60,57 @@ var getInstance = function () {
 };
 
 wishlist.prototype.getMyList = function getMyList(req, res) {
-  var wishlistModel = elements.getModel('wishlist');
+  var self = getInstance();
+  self.getListFromRequest(req, function(err, result) {
+    if(result) {
+      return res.send(result);
+    }
+    res.send(200, {});
+  });
+};
 
+wishlist.prototype.getListFromRequest = function getListFromRequest(req, callback) {
   if(req.cookies && req.cookies.wishlist) {
     // get the wishlist from the database
+    var wishlistModel = elements.getModel('wishlist');
     wishlistModel.findById(req.cookies.wishlist, function(err, result) {
-      res.send(200, result);
+      if(err) {
+        return callback(err);
+      }
+      return callback(null, result);
     });
   } else {
-    res.send(200, {});
+    return callback();
   }
 };
 
-wishlist.prototype.addToWishList = function addToWishList(req, res) {
+wishlist.prototype.addItemToWishList = function addItemToWishList(req, res, item, callback) {
+  var self = getInstance();
   var wishlistModel = elements.getModel('wishlist');
-  var userwishlist;
   var newlist = false;
+  var userwishlist;
 
   var addToWishlistObject = function addToWishlistObject(userwishlist) {
-    if(req.query.type && req.query.id) {
-      userwishlist.save(function(err, doc) {
-        if(doc && newlist) {
-          res.cookie('wishlist', doc._id);
+    if(item.type && item.id) {
+      userwishlist.listelements = [];
+      if(userwishlist.entries && userwishlist.entries[item.type]) {
+        var index = userwishlist.entries[item.type].indexOf(item.id);
+        if(index > -1) {
+          userwishlist.entries[item.type].splice(index, 1);
         }
-        doc.listelements.push(req.query.id);
-        res.send(userwishlist);
+        userwishlist.entries[item.type].push(item.id);
+      } else {
+        userwishlist.listelements.push({type: item.type, id: item.id});
+      }
+      userwishlist.save(function(err, doc) {
+        if(err) {
+          return callback(err);
+        } else {
+          if(doc && newlist) {
+            res.cookie('wishlist', doc._id);
+          }
+          return callback(null, doc);
+        }
       });
     }
   };
@@ -96,14 +121,72 @@ wishlist.prototype.addToWishList = function addToWishList(req, res) {
     addToWishlistObject(userwishlist);
   } else {
     wishlistModel.findById(req.cookies.wishlist, function(err, result) {
-      if(!err) {
+      if(err) {
+        return callback(err);
+      } else {
         userwishlist = result;
         addToWishlistObject(userwishlist);
-      } else {
-        res.send(404);
       }
     });
   }
+};
+
+wishlist.prototype.removeItemFromWishList = function removeItemFromWishList(req, res, item, callback) {
+  var self = getInstance();
+  var wishlistModel = elements.getModel('wishlist');
+  var newlist = false;
+  var userwishlist;
+
+  var removeFromWishlistObject = function removeFromWishlistObject(userwishlist) {
+    if(item.type && item.id) {
+      userwishlist.listelements = [];
+      if(userwishlist.entries && userwishlist.entries[item.type]) {
+        var index = userwishlist.entries[item.type].indexOf(item.id);
+        if(index > -1) {
+          userwishlist.entries[item.type].splice(index, 1);
+        }
+      } else {
+
+      }
+      userwishlist.save(function(err, doc) {
+        if(err) {
+          return callback(err);
+        } else {
+          if(doc && newlist) {
+            res.cookie('wishlist', doc._id);
+          }
+          return callback(null, doc);
+        }
+      });
+    }
+  };
+
+  if(req.cookies.wishlist) {
+    wishlistModel.findById(req.cookies.wishlist, function(err, result) {
+      if(err) {
+        return callback(err);
+      } else {
+        userwishlist = result;
+        removeFromWishlistObject(userwishlist);
+      }
+    });
+  }
+};
+
+wishlist.prototype.addToWishList = function addToWishList(req, res) {
+  var self = getInstance();
+  var item = {};
+  if(req.query.type && req.query.id) {
+    item.type = req.query.type;
+    item.id = req.query.id;
+  }
+  self.addItemToWishList(req, res, item, function(err, result) {
+    if(err) {
+      return res.send(404, err);
+    } else {
+      return res.send(result);
+    }
+  });
 };
 
 var init = function (m) {
